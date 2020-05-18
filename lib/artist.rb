9e00cfacd03f05298d00105313b35be380c27ecb
Aaron Schuyler <asky28@gmail.com>
2020-05-18 21:19:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,4 +14,7 @@ class Artist
   def songs
     Song.all.select {|song| song.artist == self}
   end
+  def self.find_or_create_by_name(name)
+    @@all.find {|artist| artist.name = name} 
+  end 
 end
