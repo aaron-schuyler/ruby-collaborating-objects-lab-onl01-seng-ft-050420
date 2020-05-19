9ea69878be0e9ec9ee3169cf212fcb1d7fc7991f
Aaron Schuyler <asky28@gmail.com>
2020-05-19 00:01:39 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -3,11 +3,11 @@ class MP3Importer
   attr_accessor :path, :files
   def initialize(path)
     @path = path
-    @files = Dir.entries(@path)
+    @files = Dir.entries(@path).reject{|entry| entry == “.” || entry == “…”}
   end
   def files
     @files
-    binding.pry
+    
   end
 
 end
